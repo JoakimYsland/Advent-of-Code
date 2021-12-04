41cf4e9466bc776d14ea2505d91c4e8bc81f9028
Joakim Ysland <Joakimay@gmail.com>
2021-12-04 18:34:21 +0100

    Day 4 part 1

---
+++ 2021/2021.12.04/advent_of_code_2021-12-04.py
@@ -0,0 +1,67 @@
+
+# https://adventofcode.com/2021/day/4
+
+def check_for_bingo(board, drawn_numbers):
+
+	def line_is_bingo(line):
+		is_bingo = True
+		for n in line:
+			if not n in drawn_numbers: 
+				is_bingo = False
+		if is_bingo: 
+			print("Bingo!", line)
+			return True
+		return False
+
+	# for board in boards: 
+	for row in board: 
+		if line_is_bingo(row): 
+			return True
+	for i in range(0, 5, 1):
+		column = [n for row in board for (col, n) in enumerate(row) if col == i]
+		if line_is_bingo(column): 
+			return True
+	return False
+
+def get_sum_of_unmarked_numbers(board, drawn_numbers):
+	sum = 0
+	for row in board: 
+		for n in row:
+			if not n in drawn_numbers:
+				sum += n
+	return sum
+
+# --------------------------------------------------------------------------------
+
+def run():
+	input_file = open('input.txt', 'r').readlines()
+	number_pool = [int(n.strip()) for n in input_file[0].split(',')]
+	boards = [[]]
+
+	print("number_pool", number_pool)
+
+	for line in input_file[2:]: 
+		numbers = [int(n.strip()) for n in line.split(' ') if n.strip().isdigit()]
+
+		if (len(numbers) == 0):
+			boards.append([])
+			continue
+
+		boards[-1].append(numbers)
+
+	for i, board in enumerate(boards):
+		print("board", i, board)
+
+	drawn_numbers = []
+
+	for i in range(0, len(number_pool), 1):
+		drawn_numbers.append(number_pool[i])
+		print("drawn_numbers:", drawn_numbers)
+		for board in boards: 
+			is_bingo = check_for_bingo(board, drawn_numbers)
+			if (is_bingo): 
+				sum_of_unmarked_numbers = get_sum_of_unmarked_numbers(board, drawn_numbers)
+				print("Answer", sum_of_unmarked_numbers * drawn_numbers[-1])
+				return
+
+run()
