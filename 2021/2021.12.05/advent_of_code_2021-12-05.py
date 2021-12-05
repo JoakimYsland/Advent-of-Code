e7b3e9555775560861210df124565ddb93913ec0
Joakim Ysland <Joakimay@gmail.com>
2021-12-05 16:58:23 +0100

    Update advent_of_code_2021-12-05.py

--- 2021/2021.12.05/advent_of_code_2021-12-05.py
+++ 2021/2021.12.05/advent_of_code_2021-12-05.py
@@ -10,17 +10,17 @@ def get_overlap_matrix(vent_lines):
 		x2 = max(vent_line[0], vent_line[2])
 		y = min(vent_line[1], vent_line[3])
 		y2 = max(vent_line[1], vent_line[3])
-		
+
 		# Vertical
 		if vent_line[0] == vent_line[2]: 
-			for y in range(vent_line[1], vent_line[3]+1, 1):
-				c = "{x},{y}".format(x = vent_line[0], y = y)
+			for y in range(y, y2+1, 1):
+				c = "{x},{y}".format(x = x, y = y)
 				matrix[c] = matrix[c] + 1 if (c in matrix) else 1
-
+		
 		# Horizontal
 		if vent_line[1] == vent_line[3]: 
-			for x in range(vent_line[0], vent_line[2]+1, 1):
-				c = "{x},{y}".format(x = x, y = vent_line[1])
+			for x in range(x, x2+1, 1):
+				c = "{x},{y}".format(x = x, y = y)
 				matrix[c] = matrix[c] + 1 if (c in matrix) else 1
 
 	return matrix
@@ -29,7 +29,7 @@ def get_overlap_matrix(vent_lines):
 
 def run():
 	# input_file = open('input.txt', 'r').readlines()
-	input_file = open('input_test.txt', 'r').readlines()
+	input_file = open('input.txt', 'r').readlines()
 	vent_lines = []
 
 	for line in input_file: 
@@ -38,7 +38,6 @@ def run():
 	
 	non_diagional_vent_lines = [v for v in vent_lines if v[0] == v[2] or v[1] == v[3]]
 	overlap_matrix = get_overlap_matrix(non_diagional_vent_lines)
-	print(overlap_matrix)
 
 	overlaps = len([o for o in overlap_matrix.values() if o > 1])
 	print(overlaps)
