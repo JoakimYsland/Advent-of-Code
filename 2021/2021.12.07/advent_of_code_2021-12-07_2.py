b6a4b5a303b512b72ce0704df3325109db654e6d
Joakim Ysland <Joakimay@gmail.com>
2021-12-07 17:03:32 +0100

    Update advent_of_code_2021-12-07_2.py

--- 2021/2021.12.07/advent_of_code_2021-12-07_2.py
+++ 2021/2021.12.07/advent_of_code_2021-12-07_2.py
@@ -1,11 +1,17 @@
 
 # https://adventofcode.com/2021/day/7
 
-import statistics
+def evaluate_position(target_pos, crab_data):
+	fuel = 0
+	for crab_pos in crab_data.keys(): 
+		num_crabs = crab_data[crab_pos]
+		dist = abs(target_pos - crab_pos)
+		fuel += int(dist * (dist + 1) / 2) * num_crabs # https://www.mathsisfun.com/algebra/triangular-numbers.html
+	return fuel
 
 # --------------------------------------------------------------------------------
 
-# Test / Real – 168(5) / ???
+# Test / Real – 168(5) / 88612508(447)
 
 def run(run_title, input_file):
 	crab_submarines = [int(f) for f in input_file[0].split(',')]
@@ -14,26 +20,17 @@ def run(run_title, input_file):
 	for pos in crab_submarines: 
 		crab_data[pos] = crab_data[pos] + 1 if pos in crab_data else 1
 	
-	least_fuel_consumption = -1
-	least_fuel_consumption_pos = -1
+	fuel_consumption = -1
+	position = -1
 
 	for pos in range(min(crab_submarines), max(crab_submarines)): 
-		# print("Evaluation position", pos)
-		fuel_consumption = 0
-		for crab_pos in crab_data.keys(): 
-			num_crabs = crab_data[crab_pos]
-			pos_from = min(pos, crab_pos)
-			pos_to = max(pos, crab_pos)
-			fuel_steps = [p + 1 - pos_from for p in range(pos_from, pos_to)]
-			fuel_consumption += sum(fuel_steps) * num_crabs
-			# print(crab_pos, fuel_steps, "=", sum(fuel_steps))
+		fuel = evaluate_position(pos, crab_data)
+		if (fuel_consumption == -1 or fuel < fuel_consumption):
+			fuel_consumption = fuel
+			position = pos
 
-		if (least_fuel_consumption == -1 or fuel_consumption < least_fuel_consumption):
-			least_fuel_consumption = fuel_consumption
-			least_fuel_consumption_pos = pos
-
-	print(run_title, "least_fuel_consumption:", least_fuel_consumption)
-	print(run_title, "least_fuel_consumption_pos:", least_fuel_consumption_pos)
+	print(run_title, "fuel_consumption:", fuel_consumption)
+	print(run_title, "position:", position)
 
 run("[Test]", open('input_test.txt', 'r').readlines())
 run("[Real]", open('input.txt', 'r').readlines())
