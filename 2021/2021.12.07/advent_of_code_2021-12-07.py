a060d87f19d9539323b0e941b06bdb7c99c18c6c
Joakim Ysland <Joakimay@gmail.com>
2021-12-07 13:19:32 +0100

    Create advent_of_code_2021-12-07.py

---
+++ 2021/2021.12.07/advent_of_code_2021-12-07.py
@@ -0,0 +1,24 @@
+
+# https://adventofcode.com/2021/day/7
+
+import statistics
+
+# --------------------------------------------------------------------------------
+
+# Test / Real – 37 / 326132
+
+def run(run_title, input_file):
+	crab_submarines = [int(f) for f in input_file[0].split(',')]
+	least_fuel_consumption = 0
+
+	crab_submarines.sort()
+	media_position = int(statistics.median(crab_submarines))
+
+	for sub_pos in crab_submarines: 
+		# least_fuel_consumption += abs(crab_submarines[i] - media_position)
+		least_fuel_consumption += abs(sub_pos - media_position)
+
+	print(run_title, "least_fuel_consumption:", least_fuel_consumption)
+
+run("[Test]", open('input_test.txt', 'r').readlines())
+run("[Real]", open('input.txt', 'r').readlines())
