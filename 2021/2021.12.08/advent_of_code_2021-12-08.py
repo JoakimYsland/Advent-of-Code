7a36549a73226d0d1725e4fe3ce76cf96575999a
Joakim Ysland <Joakimay@gmail.com>
2021-12-11 02:24:35 +0100

    Update advent_of_code_2021-12-08.py

--- 2021/2021.12.08/advent_of_code_2021-12-08.py
+++ 2021/2021.12.08/advent_of_code_2021-12-08.py
@@ -1,7 +1,7 @@
 
 # https://adventofcode.com/2021/day/8
 
-# import statistics
+# import re # Split string with multiple delimiters
 
 # --------------------------------------------------------------------------------
 
@@ -19,19 +19,6 @@
 # 9: 1234-67 = 6
 
 def run(run_title, input_file):
-	
-	# asd = [
-	# 	[0,1,2,4,5,6], 		# 0
-	# 	[2,5], 				# 1
-	# 	[0,2,3,4,6], 		# 2
-	# 	[0,2,3,5,6], 		# 3
-	# 	[1,2,3,5], 			# 4
-	# 	[0,1,3,5,6], 		# 5
-	# 	[0,1,3,4,5,6], 		# 6
-	# 	[0,2,5], 			# 7
-	# 	[0,1,2,3,4,5,6], 	# 8
-	# 	[0,1,2,3,5,6], 		# 9
-	# ]
 
 	# Given n number of segments, which segment
 	# could the potential digits not fill
@@ -44,29 +31,20 @@ def run(run_title, input_file):
 		[], 				# 5
 		[], 				# 6
 		[], 				# 7
-		# [], 				# 0
-		# [], 				# 1
-		# [2,5], 				# 2
-		# [0,2,5], 			# 3
-		# [1,2,3,5], 			# 4
-		# [0,1,2,3,4,5,6], 	# 5
-		# [0,1,2,3,4,5,6], 	# 6
-		# [0,1,2,3,4,5,6], 	# 7
 	]
 
 	for line in input_file:
-		mapping = [['a','b','c','d','e','f','g']] * 7
+		mapping = ['abcdefg'] * 7
 		entry = line.split(' | ')
 		signal_patterns = entry[0].split(' ')
 		output_value = entry[1].strip().split(' ')
-		print(signal_patterns, output_value)
+		
 		for pattern in signal_patterns:
 			for i in asd[len(pattern)]:
-				print("1", mapping)
-				for segment in [char for char in pattern]: 
-					if segment in mapping[i]:
-						mapping[i].remove(segment)
-				print("2", mapping)
+				for char in pattern: 
+					mapping[i] = mapping[i].replace(char, '')
+		
+		print(signal_patterns, output_value)
 		print(mapping)
 
 	# print(run_title, "least_fuel_consumption:", least_fuel_consumption)
