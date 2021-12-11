d48dbade0b510df11054602683ba95e0a213e385
Joakim Ysland <Joakimay@gmail.com>
2021-12-11 02:48:31 +0100

    Update advent_of_code_2021-12-08.py

--- 2021/2021.12.08/advent_of_code_2021-12-08.py
+++ 2021/2021.12.08/advent_of_code_2021-12-08.py
@@ -1,11 +1,9 @@
 
 # https://adventofcode.com/2021/day/8
 
-import re # Split string with multiple delimiters
-
 # --------------------------------------------------------------------------------
 
-# Test / Real – 26 / ???
+# Test / Real – 26 / 456
 
 # 0: 123-456 = 6
 # 1: --3--5- = 2 *
@@ -20,33 +18,19 @@ import re # Split string with multiple delimiters
 
 def run(run_title, input_file):
 
-	# Given <index> number of segments in the digit, 
-	# which segments can never be in the potential digit(s)
-	digit_mask = [
-		[0,1,2,3,4,5,6], 	# 0
-		[0,1,2,3,4,5,6], 	# 1
-		[0,1,3,4,6], 		# 2
-		[1,3,4,6], 			# 3
-		[0,4,6], 			# 4
-		[], 				# 5
-		[], 				# 6
-		[], 				# 7
-	]
+	unique_segment_digits = 0
 
 	for line in input_file:
 		mapping = ['abcdefg'] * 7
 		entry = line.split(' | ')
 		signal_patterns = entry[0].split(' ')
 		output_value = entry[1].strip().split(' ')
-		
-		for pattern in signal_patterns:
-			for segment in digit_mask[len(pattern)]:
-				mapping[segment] = re.sub('[' + pattern + ']', '', mapping[segment])
-		
-		print(signal_patterns, output_value)
-		print(mapping)
 
-	# print(run_title, "least_fuel_consumption:", least_fuel_consumption)
+		for pattern in output_value:
+			if len(pattern) in [2,4,3,7]:
+				unique_segment_digits += 1
+
+	print(run_title, "unique_segment_digits:", unique_segment_digits)
 
 run("[Test]", open('input_test.txt', 'r').readlines())
-# run("[Real]", open('input.txt', 'r').readlines())
+run("[Real]", open('input.txt', 'r').readlines())
