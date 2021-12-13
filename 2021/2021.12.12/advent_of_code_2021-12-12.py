9d9d006754e1a8e9e28da8cba632741ebf119a0f
Joakim Ysland <Joakimay@gmail.com>
2021-12-13 01:39:53 +0100

    Update advent_of_code_2021-12-12.py

--- 2021/2021.12.12/advent_of_code_2021-12-12.py
+++ 2021/2021.12.12/advent_of_code_2021-12-12.py
@@ -3,27 +3,25 @@
 
 # import statistics
 # from collections import deque
-from collections import namedtuple
+# from collections import namedtuple
 
 # Vec2 = namedtuple("Vec2", ['x', 'y'])
-Path = namedtuple("Path", ['steps', 'visited_small'])
 
 def run(run_title, input_file):
 
+	def can_visit(path, cave):
+		is_small_cave = not cave[0].isupper()
+		return not is_small_cave or (is_small_cave and not cave in path)
+
 	def traverse(path):
-		for connection in network[path.steps[-1]]: 
+		for connection in network[path[-1]]: 
 			if connection == 'end': 
-				new_path = Path(path.steps + [connection], path.visited_small)
+				new_path = path.copy() + [connection]
 				paths.append(new_path)
 			else: 
-				is_small_cave = not connection[0].isupper()
-				if (is_small_cave): 
-					if not path.visited_small:
-						new_path = Path(path.steps + [connection], True)
-						traverse(new_path)
-				else: 
-					new_path = Path(path.steps + [connection], path.visited_small)
-					traverse(new_path)				
+				if can_visit(path, connection):
+					new_path = path.copy() + [connection]
+					traverse(new_path)
 
 	# --------------------------------------------------------------------------------
 
@@ -42,13 +40,13 @@ def run(run_title, input_file):
 			if not opposite in network[cave] and opposite != 'start':
 				network[cave].append(opposite)
 
-	traverse(Path(['start'], False))
+	traverse(['start'])
 	total_paths = len(paths)
 
 	# for cave in network: print(cave, network[cave])
-	for path in paths: print('-'.join(path.steps))
+	for path in paths: print('-'.join(path))
 
 	print(run_title, "total_paths:", total_paths)
 
-run("[Test]", open('input_test.txt', 'r').readlines())
-# run("[Real]", open('input.txt', 'r').readlines())
+# run("[Test]", open('input_test.txt', 'r').readlines())
+run("[Real]", open('input.txt', 'r').readlines())
