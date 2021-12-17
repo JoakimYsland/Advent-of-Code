1e6ee5d4a129914a9c7304184d2f0b6b539e60bd
Joakim Ysland <Joakimay@gmail.com>
2021-12-17 03:39:38 +0100

    Update advent_of_code_2021-12-15.py

--- 2021/2021.12.15/advent_of_code_2021-12-15.py
+++ 2021/2021.12.15/advent_of_code_2021-12-15.py
@@ -5,18 +5,26 @@ import re
 from collections import namedtuple
 
 Vec2 = namedtuple("Vec2", ['x', 'y'])
-Node = namedtuple("Node", ['cost', 'total_cost'])
+Node = namedtuple("Node", ['parent', 'cost', 'total_cost'])
 
 def run(run_title, input_file):
+
+	def reconstruct_path(current, closed): 
+		path = []
+		while current != None: 
+			path.append(current)
+			current = closed[current].parent
+		path.reverse()
+		return path
 
 	def a_star_search(start, goal): 
 
 		def get_adjacent(node):
 			adjacent = []
-			if (node.x > 0): 				adjacent.append(Vec2(node.x-1, node.y))
-			if (node.x < map_size.x-1): 	adjacent.append(Vec2(node.x+1, node.y))
-			if (node.y > 0): 				adjacent.append(Vec2(node.x, node.y-1))
-			if (node.y < map_size.y-1):  	adjacent.append(Vec2(node.x, node.y+1))
+			if (node.x > 0): 			adjacent.append(Vec2(node.x-1, node.y))
+			if (node.x < map_size.x-1): adjacent.append(Vec2(node.x+1, node.y))
+			if (node.y > 0): 			adjacent.append(Vec2(node.x, node.y-1))
+			if (node.y < map_size.y-1): adjacent.append(Vec2(node.x, node.y+1))
 			return adjacent
 
 		def get_frontier_best(): 
@@ -26,7 +34,7 @@ def run(run_title, input_file):
 					best = pos
 			return best, frontier[best]
 
-		frontier = { start: Node(0,0) }
+		frontier = { start: Node(None, 0,0) }
 		closed = {}
 
 		while len(frontier) > 0: 
@@ -50,7 +58,7 @@ def run(run_title, input_file):
 					if frontier[child].total_cost < child_total_cost:
 						continue
 				
-				frontier[child] = Node(child_cost, child_total_cost)
+				frontier[child] = Node(current, child_cost, child_total_cost)
 
 		return closed
 
@@ -63,30 +71,24 @@ def run(run_title, input_file):
 	for line in input_file: 
 		risk_map.append([int(c) for c in line.strip()])
 
-	risk_map = [list(x) for x in zip(*risk_map)] # Transpose
+	# risk_map = [list(x) for x in zip(*risk_map)] # Transpose
 	map_size = Vec2(len(risk_map[0]), len(risk_map))
 	start = Vec2(0,0)
 	goal = Vec2(map_size.x-1, map_size.y-1)
 
 	closed = a_star_search(start, goal)
 
-	# print(closed)
 	for node in closed: 
 		print(node, closed[node])
 
-	# viz = [['-' for x in range(map_size.x)] for y in range(map_size.y)]
-	# for node in closed.keys():
-	# 	viz[node.x][node.y] = "#"
-	
-	# for line in viz: 
-	# 	print(''.join(line))
+	path = reconstruct_path(goal, closed)
 
-	# for i, line in enumerate(risk_map): 
-	# 	line = '-' * len(line)
-	# 	for n in (n for n in closed.keys() if n.y == i): 
-	# 		print("klasjd")
-	# 		line[n.x] == '#'
-	# 	print(line)
+	viz = [['-' for x in range(map_size.x)] for y in range(map_size.y)]
+	for pos in path: 
+		viz[pos.x][pos.y] = str(risk_map[pos.x][pos.y])
+	
+	for line in viz: 
+		print(''.join(line))
 
 run("[Test]", open('input_test.txt', 'r').readlines())
 # run("[Real]", open('input.txt', 'r').readlines())
