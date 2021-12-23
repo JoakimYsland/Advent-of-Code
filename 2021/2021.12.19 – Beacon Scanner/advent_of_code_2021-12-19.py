6b527f4d89dadb2d735b9f5a8333953108ad0e61
Joakim Ysland <Joakimay@gmail.com>
2021-12-23 01:29:08 +0100

    Create advent_of_code_2021-12-19.py

---
+++ 2021/2021.12.19 – Beacon Scanner/advent_of_code_2021-12-19.py
@@ -0,0 +1,146 @@
+
+# https://adventofcode.com/2021/day/18
+
+# import re
+import time
+import math
+from copy import deepcopy
+# from collections import namedtuple
+
+# Vec3 = namedtuple("Vec3", ['x', 'y', 'z'])
+
+class Vec3: 
+	def __init__(self, x, y, z): 
+		self.x = x
+		self.y = y
+		self.z = z
+
+	def __repr__(self): 
+		return "({0},{1},{2})".format(str(self.x), str(self.y), str(self.z))
+
+	def __sub__(self, other):
+		return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+	def __abs__(self):
+		return Vec3(abs(self.x), abs(self.y), abs(self.z))
+
+def my_print(*args, **kwargs):
+	# print(' '.join(map(str,args)), **kwargs)
+	return
+
+def run(run_title, input_file):
+
+	# --------------------------------------------------------------------------------
+
+	# Test / Real – 3993 / 4837
+
+	start_time_ms = round(time.time() * 1000)
+	
+	scanners = []
+
+	for line in input_file: 
+		if len(line) < 2: 
+			continue
+		if line.startswith('---'): 
+			scanners.append([])
+		else: 
+			x,y,z = (int(c) for c in line.strip().split(','))
+			scanners[-1].append(Vec3(x,y,z))
+
+	offsets = {}
+
+	for i in range(0, len(scanners)): 
+		for j in range(i, len(scanners)):
+			if i == j: 
+				continue
+
+			print('Comparing', i, 'and', j)
+
+			for b1 in scanners[i]:
+				x,y,z = b1 
+				b1_list = [
+					# foward, side, up
+
+					#  y -z  x
+					# -z -y  x
+					# -y  z  x
+					#  z  y  x
+
+					# -x -z  y
+					# -z  x  y
+					#  x  z  y
+					#  z -x  y
+
+					#  y  x  z
+					#  x -y  z
+					# -y -x  z
+					# -x  y  z
+
+					#  y  z -x
+					#  z -y -x
+					# -y -z -x
+					# -z  y -x
+
+					#  z  x -y
+					#  x -z -y
+					# -z -x -y
+					# -x  z -y
+
+					#  y -x -z
+					# -x -y -z
+					#  x -y -z
+					#  x  y -z
+
+					Vec3( y,-z, x), 
+					Vec3(-z,-y, x), 
+					Vec3(-y, z, x), 
+					Vec3( z, y, x), 
+
+					Vec3(-x,-z, y), 
+					Vec3(-z, x, y), 
+					Vec3( x, z, y), 
+					Vec3( z,-x, y), 
+
+					Vec3( y, x, z), 
+					Vec3( x,-y, z), 
+					Vec3(-y,-x, z), 
+					Vec3(-x, y, z), 
+
+					Vec3( y, z,-x), 
+					Vec3( z,-y,-x), 
+					Vec3(-y,-z,-x), 
+					Vec3(-z, y,-x), 
+
+					Vec3( z, x,-y), 
+					Vec3( x,-z,-y), 
+					Vec3(-z,-x,-y), 
+					Vec3(-x, z,-y), 
+
+					Vec3( y,-x,-z), 
+					Vec3(-x,-y,-z), 
+					Vec3(-y, x,-z), 
+					Vec3( x, y,-z), 
+				]
+				for b2 in scanners[j]: 
+					b2_m = Vec3(b2.y, b2.x, b2.z)
+					for b in b1_list: 
+						# offset = str(abs(b - b2_m))
+						offset = str(b - b2_m)
+						offsets.setdefault(offset, 0)
+						offsets[offset] += 1
+
+			print(len(offsets))
+			for offset, count in offsets.items(): 
+				if count > 2: 
+					print(offset, count)
+				# print(offset, count)
+
+	end_time_ms = round(time.time() * 1000)
+	total_time = end_time_ms - start_time_ms
+
+	# print('––––––––––')
+	# print(run_title, "highest_magnitude:", highest_magnitude, ('(' + str(total_time) + "ms)"))
+	# print('––––––––––')
+
+run("[Test]", open('input_test.txt', 'r').readlines())
+# run("[Real]", open('input.txt', 'r').readlines())
