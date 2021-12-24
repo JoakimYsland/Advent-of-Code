ec4fbd024cbfbc4f1ab1546fd4522ab4ba090e2d
Joakim Ysland <Joakimay@gmail.com>
2021-12-24 15:39:38 +0100

    Create advent_of_code_2021-12-20.py

---
+++ 2021/2021.12.20 – Trench Map/advent_of_code_2021-12-20.py
@@ -0,0 +1,83 @@
+
+# https://adventofcode.com/2021/day/20
+
+import time
+import math
+import re
+from copy import deepcopy
+
+def my_print(*args, **kwargs):
+	# print(' '.join(map(str,args)), **kwargs)
+	return
+
+def run(run_title, input_file):
+
+	def pad_image():
+		image.insert(0, ['0'] * len(image[0]))
+		image.append(['0'] * len(image[0]))
+		for row in image: 
+			row.insert(0, '0')
+			row.append('0')
+
+	def print_image(): 
+		for row in deepcopy(image): 
+			row_joined = ''.join(row)
+			row_joined = row_joined.replace('0', '.')
+			row_joined = row_joined.replace('1', '#')
+			print(row_joined)
+
+	def enhance(): 
+		image_copy = deepcopy(image)
+		for y in range(1, len(image) - 1): 
+			for x in range(1, len(image[0]) - 1): 
+				tile = ''
+				for r in range(0, 3): 
+					tile += ''.join(image_copy[y-1+r][x-1:x+2])
+				image[y][x] = enhancement_algorithm[int(tile, 2)]
+
+	def get_lit_pixels(): 
+		lit_pixels = 0
+		for y in range(0, len(image)): 
+			for x in range(0, len(image[0])): 
+				if image[y][x] == '1': 
+					lit_pixels += 1
+		return lit_pixels
+
+	# --------------------------------------------------------------------------------
+
+	# Test / Real – 35 / !5482
+
+	start_time_ms = round(time.time() * 1000)
+
+	enhancement_algorithm = ''
+	image = []
+
+	for i, line in enumerate(input_file): 
+		if i == 0: 
+			enhancement_algorithm = line.strip()
+			enhancement_algorithm = enhancement_algorithm.replace('.', '0')
+			enhancement_algorithm = enhancement_algorithm.replace('#', '1')
+			enhancement_algorithm = [c for c in enhancement_algorithm]
+		elif i > 1: 
+			row = line.strip()
+			row = row.replace('.', '0')
+			row = row.replace('#', '1')
+			image.append([c for c in row])
+	
+	pad_image()
+
+	for i in range(0, 2): 
+		pad_image()
+		enhance()
+
+	# print_image()
+
+	lit_pixels = get_lit_pixels()
+
+	end_time_ms = round(time.time() * 1000)
+	total_time = end_time_ms - start_time_ms
+
+	print(run_title, "lit_pixels:", lit_pixels, ('(' + str(total_time) + "ms)"))
+
+run("[Test]", open('input_test.txt', 'r').readlines())
+run("[Real]", open('input.txt', 'r').readlines())
