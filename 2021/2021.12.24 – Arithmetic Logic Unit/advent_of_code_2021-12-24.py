05fdacd0b1c69db881d04fff2523dc6685da4f31
Joakim Ysland <Joakimay@gmail.com>
2021-12-31 00:36:34 +0100

    Update advent_of_code_2021-12-24.py

--- 2021/2021.12.24 – Arithmetic Logic Unit/advent_of_code_2021-12-24.py
+++ 2021/2021.12.24 – Arithmetic Logic Unit/advent_of_code_2021-12-24.py
@@ -17,20 +17,31 @@ def run(run_title, input_file):
 		loc = get_write_location(a)
 		ALU[loc] = inps.pop(0)
 
-	def add(): 
-		print('add')
+	def add(split): 
+		a, b = split[1], get_b(split[2])
+		loc_a = get_write_location(a)
+		ALU[loc_a] = b + ALU[loc_a]
 
-	def mul(): 
-		print('mul')
+	def mul(split): 
+		a, b = split[1], get_b(split[2])
+		loc = get_write_location(a)
+		ALU[loc] *= b
 
-	def div(): 
-		print('div')
+	def div(split): 
+		a, b = split[1], get_b(split[2])
+		loc_a = get_write_location(a)
+		ALU[loc_a] = int(ALU[loc_a] / b)
 
-	def mod(): 
-		print('mod')
+	def mod(split): 
+		a, b = split[1], get_b(split[2])
+		loc_a = get_write_location(a)
+		ALU[loc_a] = ALU[loc_a] % b
 
-	def eql(): 
-		print('eql')
+	def eql(split): 
+		a, b = split[1], get_b(split[2])
+		loc_a = get_write_location(a)
+		is_equal = ALU[loc_a] == b
+		ALU[loc_a] = 1 if is_equal else 0
 	
 	def get_write_location(location): 
 		if location == 'x': return 0
@@ -47,11 +58,10 @@ def run(run_title, input_file):
 
 	# --------------------------------------------------------------------------------
 
-	# Test / Real – 44169 / !47191
+	# Test / Real – ??? / ???
 
 	start_time_ms = round(time.time() * 1000)
 
-	# ALU = { 'x': 0, 'y': 0, 'z': 0, 'w': 0 }
 	ALU = [0,0,0,0]
 	instructions = {
 		'inp': inp, 
@@ -67,91 +77,7 @@ def run(run_title, input_file):
 		split = line.strip().split()
 		instruction = split[0]
 
-		if instruction == 'inp': 
-			# a = split[1]
-			# loc = get_write_location(a)
-			# ALU[loc] = inps.pop(0)
-			instructions[instruction](split)
-
-		elif instruction == 'add': 
-			a, b = split[1], get_b(split[2])
-			loc_a = get_write_location(a)
-			ALU[loc_a] = b + ALU[loc_a]
-
-		elif instruction == 'mul': 
-			a, b = split[1], get_b(split[2])
-			loc = get_write_location(a)
-			ALU[loc] *= b
-
-		elif instruction == 'div': 
-			a, b = split[1], get_b(split[2])
-			loc_a = get_write_location(a)
-			ALU[loc_a] = int(ALU[loc_a] / b)
-
-		elif instruction == 'mod': 
-			a, b = split[1], get_b(split[2])
-			loc_a = get_write_location(a)
-			ALU[loc_a] = ALU[loc_a] % b
-
-		elif instruction == 'eql': 
-			a, b = split[1], get_b(split[2])
-			loc_a = get_write_location(a)
-			is_equal = ALU[loc_a] == b
-			ALU[loc_a] = 1 if is_equal else 0
-
-	# burrow_init = [['', '', '', '', '', '', '', '', '', '', '']] # Hallway (0 - left, 10 - right)
-	# # movement_cost = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }
-	# # total_cost = 0
-
-	# # -1-3-5-7-9-
-	# #   C B D D  => A B C D
-	# #   D C B A  => A B C D
-	# #   D B A C  => A B C D
-	# #   B C A A  => A B C D
-	# burrow_init.append(['C', 'D', 'D', 'B']) # Room 1 (0 - top, 1 - bottom)
-	# burrow_init.append(['B', 'C', 'B', 'C']) # Room 1 (0 - top, 1 - bottom)
-	# burrow_init.append(['D', 'B', 'A', 'A']) # Room 1 (0 - top, 1 - bottom)
-	# burrow_init.append(['D', 'A', 'C', 'A']) # Room 1 (0 - top, 1 - bottom)
-
-	# lowest_score = 999999
-	# valid_hallway = [0,1,3,5,7,9,10]
-
-	# def try_stuff(burr, score): 
-
-	# 	nonlocal lowest_score
-	# 	if score >= lowest_score: 
-	# 		return
-
-	# 	# Enter
-	# 	for i in valid_hallway: 
-	# 		if burr[0][i] == '': 
-	# 				continue
-	# 		for j in range(1, 5): 
-	# 			b = deepcopy(burr)
-	# 			success, cost = enter(i,j, b)
-	# 			if success == True: 
-	# 				new_score = score + cost
-	# 				if new_score < lowest_score: 
-	# 					is_complete = len(''.join(b[0])) == 0
-	# 					if is_complete: 
-	# 						lowest_score = new_score
-	# 						print('lowest_score', lowest_score)
-	# 					else: 
-	# 						try_stuff(b, new_score)
-				
-	# 	# Leave
-	# 	for j in valid_hallway: 
-	# 		if burr[0][j] != '': 
-	# 				continue
-	# 		for i in range(1, 5): 
-	# 			b = deepcopy(burr)
-	# 			success, cost = leave(i,j, b)
-	# 			if success == True: 
-	# 				new_score = score + cost
-	# 				if new_score < lowest_score: 
-	# 					try_stuff(b, new_score)
-	
-	# try_stuff(burrow_init, 0)
+		instructions[instruction](split)
 
 	end_time_ms = round(time.time() * 1000)
 	total_time = end_time_ms - start_time_ms
