e2929612f23d6acd6cce62ab9bc4b359dba08d48
Joakim Ysland <Joakimay@gmail.com>
2021-12-30 15:07:41 +0100

    Update advent_of_code_2021-12-23_2.py

--- 2021/2021.12.23 – Amphipod/advent_of_code_2021-12-23_2.py
+++ 2021/2021.12.23 – Amphipod/advent_of_code_2021-12-23_2.py
@@ -7,157 +7,147 @@ import re
 from copy import deepcopy
 
 def my_print(*args, **kwargs):
-	print(' '.join(map(str,args)), **kwargs)
+	# print(' '.join(map(str,args)), **kwargs)
 	return
 
 def run(run_title, input_file):
 
-	def is_hallway_clear(from_index, to_index): 
+	def is_hallway_clear(from_index, to_index, burrow): 
 		a = min(from_index, to_index)
 		b = max(from_index, to_index)
 		occupied_slots = [e for e in burrow[0][a:b] if e != '']
 		return len(occupied_slots) == 0
 
-	def leave(from_room_index, hallway_index): 
-		nonlocal total_cost
+	def leave(from_room_index, hallway_index, burrow): 
 		num_vacant = len([s for s in burrow[from_room_index] if s == ''])
+
+		if num_vacant == len(burrow[from_room_index]): 
+			return False, 0
+
 		amphipod = burrow[from_room_index][num_vacant]
+		if amphipod != 'A' and amphipod != 'B' and amphipod != 'C' and amphipod != 'D': 
+			return False, 0
+
 		burrow[from_room_index][num_vacant] = ''
 		cost_exit_room = num_vacant + 1
 
 		from_room_entrance_index = from_room_index * 2
 		cost_hallway_move = abs(hallway_index - from_room_entrance_index)
 		cost_final = (cost_exit_room + cost_hallway_move) * movement_cost[amphipod]
-		total_cost += cost_final
-		s = "Move {0} from R{1} to H{2}"
-		result = s.format(amphipod, from_room_index, hallway_index)
+		# s = "Move {0} from R{1} to H{2}"
+		# result = s.format(amphipod, from_room_index, hallway_index)
 
-		if not is_hallway_clear(from_room_entrance_index, hallway_index): 
-			s = " – ERROR: Hallway {0} to {1} is blocked!"
-			result += s.format(from_room_entrance_index, hallway_index)
+		if not is_hallway_clear(from_room_entrance_index, hallway_index, burrow): 
+			# s = " – ERROR: Hallway {0} to {1} is blocked!"
+			# result += s.format(from_room_entrance_index, hallway_index)
+			return False, 0
 
 		burrow[0][hallway_index] = amphipod
 
-		my_print(result)
+		return True, cost_final
 	
-	def enter(hallway_index, to_room_index): 
-		nonlocal total_cost
+	def enter(hallway_index, to_room_index, burrow): 
 		amphipod = burrow[0][hallway_index]
-
 		num_vacant = len([s for s in burrow[to_room_index] if s == ''])
+		
+		if num_vacant == 0: 
+			return False, 0
+		if amphipod == '': 
+			return False, 0
+		if to_room_index == 1 and amphipod != 'A': 
+			return False, 0
+		if to_room_index == 2 and amphipod != 'B': 
+			return False, 0
+		if to_room_index == 3 and amphipod != 'C': 
+			return False, 0
+		if to_room_index == 4 and amphipod != 'D': 
+			return False, 0
+		for a in burrow[to_room_index]: 
+			if a != '' and a.upper() != amphipod: 
+				return False, 0
+
 		cost_enter_room = num_vacant
-		burrow[to_room_index][num_vacant - 1] = amphipod
 
 		to_room_entrance_index = to_room_index * 2
 		cost_hallway_move = abs(hallway_index - to_room_entrance_index)
 		cost_final = (cost_hallway_move + cost_enter_room) * movement_cost[amphipod]
 		burrow[0][hallway_index] = ''
-		total_cost += cost_final
-		s = "Move {0} from H{1} to R{2}"
-		result = s.format(amphipod, hallway_index, to_room_index)
+		# s = "Move {0} from H{1} to R{2}"
+		# result = s.format(amphipod, hallway_index, to_room_index)
 
-		if not is_hallway_clear(hallway_index, to_room_entrance_index): 
-			s = " – ERROR: Hallway {0} to {1} is blocked!"
-			result += s.format(hallway_index, to_room_entrance_index)
+		burrow[to_room_index][num_vacant - 1] = amphipod.lower()
 
-		my_print(result)
+		if not is_hallway_clear(hallway_index, to_room_entrance_index, burrow): 
+			# s = " – ERROR: Hallway {0} to {1} is blocked!"
+			# result += s.format(hallway_index, to_room_entrance_index)
+			return False, 0
 
-	def transfer(from_room_index, to_room_index): 
-		nonlocal total_cost
-		num_vacant = len([s for s in burrow[from_room_index] if s == ''])
-		amphipod = burrow[from_room_index][num_vacant]
-		burrow[from_room_index][num_vacant] = ''
-		cost_exit_room = num_vacant + 1
+		return True, cost_final
 
-		num_vacant = len([s for s in burrow[to_room_index] if s == ''])
-		cost_enter_room = num_vacant
-		burrow[to_room_index][num_vacant - 1] = amphipod
-
-		from_room_entrance_index = from_room_index * 2
-		to_room_entrance_index = to_room_index * 2
-		cost_hallway_move = abs(from_room_entrance_index - to_room_entrance_index)
-		cost_final = (cost_exit_room + cost_hallway_move + cost_enter_room) * movement_cost[amphipod]
-		total_cost += cost_final
-		s = "Transfer {0} from R{1} to R{2}"
-		result = s.format(amphipod, from_room_index, to_room_index)
-
-		if not is_hallway_clear(from_room_entrance_index, to_room_entrance_index): 
-			s = " – ERROR: Hallway {0} to {1} is blocked!"
-			result += s.format(from_room_entrance_index, to_room_entrance_index)
-
-		my_print(result)
-
-	def visualize_burrow(): 
+	def visualize_burrow(burrow): 
 		print(str(burrow[0]).replace('\'\'', '-').replace('\'', '').replace(',', ''))
 		for r in [list(x) for x in zip(*burrow[1:5])]: 
 			print("    " + str(r).replace('\'\'', '\'-\'').replace('\'', '').replace(',', '  '))
 
 	# --------------------------------------------------------------------------------
 
-	# Test / Real – 44169 / ???
+	# Test / Real – 44169 / !47191
 
 	start_time_ms = round(time.time() * 1000)
 	
-	burrow = [['', '', '', '', '', '', '', '', '', '', '']] # Hallway (0 - left, 10 - right)
+	burrow_init = [['', '', '', '', '', '', '', '', '', '', '']] # Hallway (0 - left, 10 - right)
 	movement_cost = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }
 	total_cost = 0
-
-	# # Example (44169)
-	# burrow.append(['B', 'D', 'D', 'A']) # Room 1 (0 - top, 1 - bottom)
-	# burrow.append(['C', 'C', 'B', 'D']) # Room 1 (0 - top, 1 - bottom)
-	# burrow.append(['B', 'B', 'A', 'C']) # Room 1 (0 - top, 1 - bottom)
-	# burrow.append(['D', 'A', 'C', 'A']) # Room 1 (0 - top, 1 - bottom)
-
-	# print(burrow)
-	# leave(4,10) 	# D
-	# leave(4,0) 		# A
-	# leave(3,9) 		# B
-	# leave(3,7) 		# B
-	# leave(3,1) 		# A
-	# transfer(2,3) 	# C **
-	# transfer(2,3) 	# C ***
-	# leave(2,5) 		# B
-	# leave(2,3) 		# D
-	# enter(5,2) 		# B *
-	# enter(7,2) 		# B **
-	# enter(9,2) 		# B ***
-	# transfer(4,3) 	# C ****
-	# leave(4,9) 		# A
-	# enter(3,4) 		# D *
-	# transfer(1,2) 	# B ****
-	# transfer(1,4) 	# D **
-	# transfer(1,4) 	# D ***
-	# enter(1,1) 		# A **
-	# enter(0,1) 		# A ***
-	# enter(9,1) 		# A ****
-	# enter(10,4) 	# D ****
-	# print(burrow)
 
 	# -1-3-5-7-9-
 	#   C B D D  => A B C D
 	#   D C B A  => A B C D
 	#   D B A C  => A B C D
 	#   B C A A  => A B C D
-	burrow.append(['C', 'D', 'D', 'B']) # Room 1 (0 - top, 1 - bottom)
-	burrow.append(['B', 'C', 'B', 'C']) # Room 1 (0 - top, 1 - bottom)
-	burrow.append(['D', 'B', 'A', 'A']) # Room 1 (0 - top, 1 - bottom)
-	burrow.append(['D', 'A', 'C', 'A']) # Room 1 (0 - top, 1 - bottom)
+	burrow_init.append(['C', 'D', 'D', 'B']) # Room 1 (0 - top, 1 - bottom)
+	burrow_init.append(['B', 'C', 'B', 'C']) # Room 1 (0 - top, 1 - bottom)
+	burrow_init.append(['D', 'B', 'A', 'A']) # Room 1 (0 - top, 1 - bottom)
+	burrow_init.append(['D', 'A', 'C', 'A']) # Room 1 (0 - top, 1 - bottom)
 
-	leave(3,10)
-	leave(3,9)
-	leave(3,0)
-	leave(3,1)
+	lowest_score = 999999
+	valid_hallway = [0,1,3,5,7,9,10]
 
-	transfer(1,3)
-	leave(2,3)
-	transfer(2,3)
-	leave(2,7)
-	transfer(2,3)
-	enter(3,2)
-	enter(7,2)
-	enter(9,2)
+	def try_stuff(burr, score): 
 
-	visualize_burrow()
+		nonlocal lowest_score
+		if score >= lowest_score: 
+			return
+
+		# Enter
+		for i in valid_hallway: 
+			if burr[0][i] == '': 
+					continue
+			for j in range(1, 5): 
+				b = deepcopy(burr)
+				success, cost = enter(i,j, b)
+				if success == True: 
+					new_score = score + cost
+					if new_score < lowest_score: 
+						is_complete = len(''.join(b[0])) == 0
+						if is_complete: 
+							lowest_score = new_score
+							print('lowest_score', lowest_score)
+						else: 
+							try_stuff(b, new_score)
+				
+		# Leave
+		for j in valid_hallway: 
+			if burr[0][j] != '': 
+					continue
+			for i in range(1, 5): 
+				b = deepcopy(burr)
+				success, cost = leave(i,j, b)
+				if success == True: 
+					new_score = score + cost
+					if new_score < lowest_score: 
+						try_stuff(b, new_score)
+	
+	try_stuff(burrow_init, 0)
 
 	end_time_ms = round(time.time() * 1000)
 	total_time = end_time_ms - start_time_ms
