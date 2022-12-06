8551bd6cc3cc1c2f916d0b50bdbe30fc27cb2d7d
Joakim Ysland <Joakimay@gmail.com>
2022-12-06 23:43:13 +0100

    Day 6

---
+++ 2022/2022.12.06 – Tuning Trouble/advent_of_code_2021-12-06.py
@@ -0,0 +1,21 @@
+
+# https://adventofcode.com/2022/day/5
+
+import string
+
+# input_file = open('test_input.txt', 'r').readlines()
+input_file = open('input.txt', 'r').readlines()
+
+buffer = ""
+marker_length = 4
+
+for line in input_file:
+
+	line = line.rstrip()
+
+	for i in range(marker_length - 1, len(line), 1): 
+		seq = line[i-(marker_length - 1):i+1]
+		is_marker = len(set(seq)) == len(seq)
+		if is_marker:
+			print(seq, i + 1)
+			break
