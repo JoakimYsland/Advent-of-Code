3659bc3a1f14c7893f856eb850e3ee530d6a2601
Joakim Ysland <Joakimay@gmail.com>
2022-12-23 13:59:46 +0100

    Update Snippets.py

--- Snippets.py
+++ Snippets.py
@@ -1,5 +1,25 @@
 
 # 2D List
-e = '.'
 width, height = 10, 10
-cave = [[e] * width for i in range(height)]
+cave = [['.'] * width for i in range(height)]
+
+class Vec2:
+	def __init__(self, x, y): 
+		self.x = x
+		self.y = y
+	def __str__(self):
+		s = "{0},{1}"
+		return s.format(self.x, self.y)
+	def __add__(self, v): 
+		return Vec2(self.x + v.x, self.y + v.y)
+
+class Vec3:
+	def __init__(self, x, y, z): 
+		self.x = x
+		self.y = y
+		self.z = z
+	def __str__(self):
+		s = "{0},{1},{2}"
+		return s.format(self.x, self.y, self.z)
+	def __add__(self, v): 
+		return Vec2(self.x + v.x, self.y + v.y, self.z + v.z)
