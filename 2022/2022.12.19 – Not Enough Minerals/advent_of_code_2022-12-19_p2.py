005275ffb20ced03de279b7e92b1b7b87fb66ed4
Joakim Ysland <Joakimay@gmail.com>
2022-12-24 02:35:56 +0100

    Part 2

--- 2022/2022.12.19 – Not Enough Minerals/advent_of_code_2022-12-19_p2.py
+++ 2022/2022.12.19 – Not Enough Minerals/advent_of_code_2022-12-19_p2.py
@@ -6,6 +6,7 @@ import math
 import time
 import datetime
 import re
+import numpy
 from copy import deepcopy
 
 def prt(*args, **kwargs):
@@ -40,16 +41,23 @@ def build(robot_id, robot, fleet, inventory):
 		inventory[i] -= robot[i]
 	fleet[robot_id] += 1
 
+def get_branch_potential(bp, fleet, inventory, remaining_time): 
+	max_geodes = inventory[3] # Existing geodes
+	max_geodes += fleet[3] * remaining_time # What current robots will produce
+	# Using triangle numbers, assume we build another 
+	# Geode Robot for every single remaining round
+	max_geodes += int((remaining_time * (remaining_time + 1)) / 2) 
+	return max_geodes
+
 # input_file = open('test_input.txt', 'r').readlines()
-input_file = open('input.txt', 'r').readlines()
+# input_file = open('input.txt', 'r').readlines()
+input_file = open('input_p2.txt', 'r').readlines()
 
 start_time_ms = round(time.time() * 1000)
 print("Start time:", get_time_now())
 
 blueprints = []
 most_geodes = 0
-robot_build_priority = [0,1,2,3]
-# robot_build_priority = [3,2,1,0]
 
 for i, line in enumerate(input_file):
 
@@ -71,11 +79,16 @@ def run_branch(bp, fleet, inventory, remaining_time, robot_id):
 	def end_branch(): 
 		global most_geodes
 		most_geodes = max(most_geodes, inventory[3])
-		# print(remaining_time, fleet, inventory)
 
+	global most_geodes
 	global num_branches
 
 	robot_cost = bp.robots[robot_id]
+
+	# Check if the branch has any chance of producing a high-score
+	branch_potential = get_branch_potential(bp, fleet, inventory, remaining_time)
+	if branch_potential <= most_geodes: 
+		return
 
 	# Return if the current fleet will 
 	# never be able to build the target robot
@@ -137,27 +150,25 @@ def run_branch(bp, fleet, inventory, remaining_time, robot_id):
 	end_branch()
 
 num_branches = 4
-quality_levels = []
+most_geodes_all = []
 
 def simulate_blueprint(i): 
 	global most_geodes
-	global quality_levels
 	most_geodes = 0
-	remaining_time = 24
+	remaining_time = 32
 	# print_blueprint(blueprints[i])
 	run_branch(blueprints[i], [1,0,0,0], [0,0,0,0], remaining_time, 0)
 	run_branch(blueprints[i], [1,0,0,0], [0,0,0,0], remaining_time, 1)
 	run_branch(blueprints[i], [1,0,0,0], [0,0,0,0], remaining_time, 2)
 	run_branch(blueprints[i], [1,0,0,0], [0,0,0,0], remaining_time, 3)
-	quality_level = blueprints[i].bp_id * most_geodes
-	quality_levels.append(quality_level)
+	most_geodes_all.append(most_geodes)
 
 for i in range(0, len(blueprints)): 
 	simulate_blueprint(i)
 
-print("most_geodes:", most_geodes) # 9
-print("quality_levels:", sum(quality_levels))
-print("num_branches:", num_branches) # 72419
+print("most_geodes:", max(most_geodes_all))
+print("most_geodes_prod:", numpy.prod(most_geodes_all))
+print("num_branches:", num_branches)
 
 end_time_ms = round(time.time() * 1000)
 print("End time:", get_time_now())
