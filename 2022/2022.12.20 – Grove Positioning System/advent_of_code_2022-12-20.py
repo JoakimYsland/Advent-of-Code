f0567f5ce90af09fe2be41ad3deaf68fa65d9956
Joakim Ysland <Joakimay@gmail.com>
2022-12-26 22:49:09 +0100

    Day 20 part 1

---
+++ 2022/2022.12.20 – Grove Positioning System/advent_of_code_2022-12-20.py
@@ -0,0 +1,104 @@
+
+# https://adventofcode.com/2022/day/20
+
+import string
+import math
+import time
+import datetime
+import re
+from copy import deepcopy
+from collections import namedtuple
+
+def prt(*args, **kwargs):
+	print(' '.join(map(str,args)), **kwargs)
+	return
+
+def get_time_now(): 
+	return datetime.datetime.now().strftime("%H:%M:%S")
+
+class Number: 
+	def __init__(self, value, mixed): 
+		self.value = value
+		self.mixed = mixed
+	def __str__(self): 
+		s = "{0}, {1}"
+		return s.format(self.value, self.mixed)
+
+# input_file = open('test_input.txt', 'r').readlines()
+# input_file = open('test_input2.txt', 'r').readlines()
+input_file = open('input.txt', 'r').readlines()
+
+device_file = []
+
+start_time_ms = round(time.time() * 1000)
+print("Start time:", get_time_now())
+
+for i, line in enumerate(input_file):
+
+	line = line.rstrip()
+	device_file.append(Number(int(line), False))
+
+def print_sequence(): 
+	numbers = []
+	for n in device_file: 
+		numbers.append(str(n.value))
+	print(', '.join(numbers))
+
+def get_nth(i): 
+	i %= len(device_file)
+	return device_file[i]
+
+def mix():
+	num_mixes = 0
+	while num_mixes < len(device_file): 
+		for i, number in enumerate(device_file): 
+
+			# Only mix each number once
+			if number.mixed == True: 
+				continue
+
+			num_mixes += 1
+
+			# No need to do mixing if the number is 0
+			if number.value == 0: 
+				number.mixed = True
+				continue
+
+			# Do the mixing
+			n = device_file.pop(i)
+			n.mixed = True
+			new_index = (i + number.value) % len(device_file)
+			device_file.insert(new_index, n)
+
+			# Debug text
+			# s = "{0} moves between {1} and {2} ({3})"
+			# left = device_file[(new_index-1) % len(device_file)].value
+			# right = device_file[(new_index+1) % len(device_file)].value
+			# print(s.format(n.value, left, right, new_index))
+			# print_sequence()
+
+			# Only mix 1 number at a time
+			break
+
+# print_sequence()
+mix()
+
+# Get index of '0' value
+zero_index = None
+for i, n in enumerate(device_file): 
+	if n.value == 0: 
+		zero_index = i
+		break
+
+# Find answer
+key_numbers = [1000, 2000, 3000]
+for i, n in enumerate(key_numbers): 
+	key_numbers[i] = get_nth(n + zero_index).value
+answer = sum(key_numbers)
+
+print("key_numbers:", key_numbers)
+print("answer:", answer)
+
+end_time_ms = round(time.time() * 1000)
+print("End time:", get_time_now())
+print("Total time:", end_time_ms - start_time_ms, "ms")
